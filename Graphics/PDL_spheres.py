3b122239a7cb221c2be6491800300cfaf63bf733
NRubin <noahrubin@seas.harvard.edu>
2017-07-25 17:31:19 -0400

    Adding scripts to plot Poincare Spheres.

---
+++ Graphics/PDL_spheres.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 17:29:36 2017
+
+This script is intended to make nice plots of poincare spheres in the presence of PDL, in the spirit of Damask pp. 305.
+
+@author: Noah
+"""
+
