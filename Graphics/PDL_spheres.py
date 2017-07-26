51c2bb03aff80fed32e1b89d4d1b85a4ee50a250
NRubin <noahrubin@seas.harvard.edu>
2017-07-26 14:07:52 -0400

    Added sphere rotation effect.

--- Graphics/PDL_spheres.py
+++ Graphics/PDL_spheres.py
@@ -7,7 +7,6 @@ This script is intended to make nice plots of poincare spheres in the presence o
 @author: Noah
 """
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.patches import FancyArrowPatch, ArrowStyle
@@ -57,12 +56,19 @@ x = np.cos(u)*np.sin(v)
 y = np.sin(u)*np.sin(v)
 z = np.cos(v)
 
-wireframe_plot(x, y, z, 221, fig)  # plot the data
+wireframe_plot(x, y, z, 231, fig)  # plot the data
 
 #%% Now consider spherical plot of intensities under diattenuation
+tx = 0.75
+ty = 0.5
 
-alpha = 0.345  # define the attenuation factor
-direction = [0, -1, 0]  # define the direction of diattentuation
+# construct the Jones matrix of the diattenuator
+diattenuator = np.array([[tx, 0], [0, ty]])
+
+# compute the attenuation factor
+alpha = -np.log(ty/tx)  
+
+direction = [1, 0, 0]  # define the direction of diattentuation
 gamma = np.tanh(alpha)
 
 unit_matrix = np.ones(np.shape(x))
@@ -74,7 +80,7 @@ dot_product = (x*direction[0] + y*direction[1]+
 trans = 1/(1+gamma) * (1+gamma*dot_product)
 
 # now plot the result
-wireframe_plot(trans*x, trans*y, trans*z, 222, fig)
+wireframe_plot(trans*x, trans*y, trans*z, 232, fig)
 
 #%% Now compute the change in polarization state plot
 
@@ -88,11 +94,46 @@ new_y = coeff_1 * y + coeff_2 * direction[1]
 new_z = coeff_1 * z + coeff_2 * direction[2]
 
 # plot the result
-wireframe_plot(new_x, new_y, new_z, 223, fig)
+wireframe_plot(new_x, new_y, new_z, 233, fig)
 
 #%% Show a plot which shows polarization change, as well as the change in intensity
 
-wireframe_plot(trans*new_x, trans*new_y, trans*new_z, 224,                                                                      fig)
+fin_x = trans*new_x
+fin_y = trans*new_y
+fin_z = trans*new_z
+
+wireframe_plot(fin_x, fin_y, fin_z, 234, fig)
+
+#%% Now simulate the effect of a retarder by rotating the resultant sphere
+
+retardance = np.pi/2
+ret_axis = [1, 0, 0] # retards about the x axis
+ret_axis = ret_axis/np.linalg.norm(ret_axis)
+
+shape_array = np.shape(fin_x)
+rot_x = np.zeros(shape_array)
+rot_y = np.zeros(shape_array)
+rot_z = np.zeros(shape_array)
+
+for i in range(shape_array[0]):
+    for j in range(shape_array[1]):
+      curr_x = fin_x[i, j]
+      curr_y = fin_y[i, j]
+      curr_z = fin_z[i, j]
+      
+      vect = np.array([curr_x, curr_y, curr_z])
+      first_term = np.dot(ret_axis, vect)*ret_axis
+      second_term = np.sin(retardance)*np.cross(ret_axis,
+             vect)
+      third_term = -np.cos(retardance)*np.cross(ret_axis,
+             np.cross(ret_axis, vect))
+      
+      final_vect = first_term + second_term + third_term
+      rot_x[i,j] = final_vect[0]
+      rot_y[i,j] = final_vect[1]
+      rot_z[i,j] = final_vect[2]
+
+wireframe_plot(rot_x, rot_y, rot_z, 235, fig)
 
 #%% A class found on StackExchange for drawing 3D arrows in matplotlib
 
